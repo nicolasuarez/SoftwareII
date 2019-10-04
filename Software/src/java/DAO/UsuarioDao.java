308b912b9096afed392ee4047c7161de71c32269
luisEscobar1 <grifo279@gmail.com>
2019-10-04 16:45:34 -0500

    Dao usuario , metodo insert, y buscar

--- Software/src/java/DAO/UsuarioDao.java
+++ Software/src/java/DAO/UsuarioDao.java
@@ -9,13 +9,11 @@ import java.sql.*;
 import Model.Usuario;
 import java.util.List;
 
-
-
 /**
  *
  * @author User
  */
-public class UsuarioDao implements IBaseDatos<Usuario>  {
+public class UsuarioDao implements IBaseDatos<Usuario> {
 
     @Override
     public List<Usuario> findAll() throws SQLException {
@@ -24,12 +22,13 @@ public class UsuarioDao implements IBaseDatos<Usuario>  {
 
     @Override
     public boolean insert(Usuario t) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        boolean result = false;
+        boolean result;
+        result = false;
         Connection connection = Conexion.getConnection();
-        String query = "Usuarios values (IdUsu,Usu,Contreseña,Nombre,Apellido,Tipo,genero), values (?,?,?,?,?,?) ";
+        String query = "insert into Usuarios  (IdUsu,Usu,Contreseña,Nombre,Apellido,Tipo,sexo,Correo) values (?,?,?,?,?,?,?,?) ";
         PreparedStatement preparedStmt = null;
         try {
+            System.out.println("sdsdsdsd");
             preparedStmt = connection.prepareStatement(query);
             preparedStmt.setInt(1, t.getId());
             preparedStmt.setString(2, t.getUsuario());
@@ -38,13 +37,52 @@ public class UsuarioDao implements IBaseDatos<Usuario>  {
             preparedStmt.setString(5, t.getApellido());
             preparedStmt.setString(6, "Estudiante");
             preparedStmt.setString(7, t.getSexo());
+            preparedStmt.setString(8, t.getCorreo());
             result = preparedStmt.execute();
+            System.out.println(result);
         } catch (SQLException e) {
-            e.printStackTrace();
+               e.printStackTrace();
         }
 
         return result;
 
+    }
+
+    public Usuario find(String user) throws SQLException {
+        Usuario resultado = null;
+        System.out.println(user);
+
+        String query = "Select * from Usuarios Where Usu = '" + user + "'";
+
+        System.out.println(query);
+        System.out.println("----");
+        Connection connection = Conexion.getConnection();
+        try {
+            System.out.println("entro");
+            try (Statement st = connection.createStatement()) {
+                ResultSet rs = st.executeQuery(query);
+                int id = 0;
+                String nombre = null, apellido = null;
+                String usuario = null, clave = null, correo = null;
+                String tipo = null, sexo = null;
+                if (rs.next()) {
+                    id = rs.getInt("idUSu");
+                    usuario = rs.getString("Usu");
+                    clave = rs.getString("Contreseña");
+                    nombre = rs.getString("Nombre");
+                    apellido = rs.getString("Apellido");
+                    tipo = rs.getString("Tipo");
+                    sexo = rs.getString("Sexo");
+                    correo = rs.getString("Correo");
+                    System.out.println(id);
+                    resultado = new Usuario();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Problemas al obtener persona");
+        }
+
+        return resultado;
     }
 
     @Override
