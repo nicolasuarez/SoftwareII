79a49665b922dea95918d5d45e0e36edfb3a4012
luisEscobar1 <41587880+luisEscobar1@users.noreply.github.com>
2019-11-11 20:37:57 -0500

    Revert "Luis e"

--- base1.sql
+++
@@ -1,6 +0,0 @@
-
-
-use Desercion;
-insert into administrador (id_Adm,usuario,clave,nombre,apellido)  values (32131,'esc1','312','avel','esc','Estudiante','oter','grifo1@c.co');
-SELECT * FROM usuarios ; 
-
